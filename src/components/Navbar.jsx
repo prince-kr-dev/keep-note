455f46fbc5293a43de6ba00bf381e2b48ff386f2
Prince Kumar <princekumark2004@gmail.com>
2025-07-10 09:05:54 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,6 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-center gap-10 bg-gray-900 border-b border-gray-700 py-5 text-white">
       <NavLink
-        to={"/notes"}
-        className={({ isActive }) =>
-          `text-xl md:text-2xl font-medium transition ${
-            isActive ? "text-violet-500" : "hover:text-violet-400"
-          }`
-        }
-      >
-        Notes
-      </NavLink>
-      <NavLink
         to={"/"}
         className={({ isActive }) =>
           `text-xl md:text-2xl font-medium transition ${
@@ -22,6 +12,16 @@ export default function Navbar() {
         }
       >
         Create
+      </NavLink>
+      <NavLink
+        to={"/notes"}
+        className={({ isActive }) =>
+          `text-xl md:text-2xl font-medium transition ${
+            isActive ? "text-violet-500" : "hover:text-violet-400"
+          }`
+        }
+      >
+        Notes
       </NavLink>
     </nav>
   );
